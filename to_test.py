b9fb0b3a005a63635acec1b88bf5993e330c3921
kater <kater@mail.ru>
2023-02-06 18:02:30 +0300

    test_to_nimfa

--- to_test.py
+++ to_test.py
@@ -1,4 +1,3 @@
-
 from random import randint
 
 general_time = 0
@@ -7,39 +6,108 @@ longevity = randint (15, 80)
 considering_period = 30
 min_marry_age = 16 
 
+Nimfa_Table_1 = list ()
 General_Table = list ()
+Adult_Table_1_n = list ()
 
 def creater_new_animals(number, longevity, general_time=0):
     for index  in range(general_time, number, 1):
-        sex = randint(0, 1)
+        sex = 1
         #завести в переменную
         data_birth = general_time + randint(0, 25)
         data_death = data_birth + longevity
         animal = (len(General_Table), None, None, sex, data_birth, data_death)
         General_Table.append (animal)
+        Nimfa_Table_1.append(animal)
 
-def cleaner(Cunner_Table, const, nomber = 5):
-    for index  in range (len(Cunner_Table)):
-            if Cunner_Table [index][nomber] > const:
-                Cunner_Table.pop(index)
-            index = max(index - 1, 0)
+for index  in range (len(Nimfa_Table_1)):
+    Servis = list()
+    if general_time - Nimfa_Table_1 [index][4] >= min_marry_age:
+        Adult_Table_1_n.append(Nimfa_Table_1 [index])
+    else:
+        Servis.append(Nimfa_Table_1 [index])
+    Nimfa_Table_1 = Servis
+
+creater_new_animals (20, 30)
+
+print(General_Table)
+print("********************")
+print(Nimfa_Table_1)
+print("********************")
+print(Adult_Table_1_n)
+
+general_time = 50
+Servis = list()
+for index  in range (len(Nimfa_Table_1)):
+    print("len(Nimfa_Table_1) = ", len(Nimfa_Table_1))
+    print("index = ", index)
+    print("general_time = ", general_time)
+    print("Nimfa_Table_1 [index][4] = ", Nimfa_Table_1 [index][4])
+    if general_time - Nimfa_Table_1 [index][4] >= min_marry_age:
+        Adult_Table_1_n.append(Nimfa_Table_1 [index])
+        print(general_time - Nimfa_Table_1 [index][4], ">=", min_marry_age)
+        print(Adult_Table_1_n)
+    else:
+        Servis.append(Nimfa_Table_1 [index])
+        print (Nimfa_Table_1 [index])
+    print("**********************")
+Nimfa_Table_1 = Servis
+
+print("Adult_Table_1_n ", len(Adult_Table_1_n))
+print(Adult_Table_1_n)
+print("Nimfa_Table_1  ", len(Nimfa_Table_1))
+print(Nimfa_Table_1)
 
 
-creater_new_animals (10, 60)
+# Первая проблема, выход 
+# def cleaner(Cunner_Table, const, nomber = 5):
+#     for index  in range (len(Cunner_Table)):
+#             if Cunner_Table [index][nomber] > const:
+#                 Cunner_Table.pop(index)
+#             index = max(index - 1, 0)
 
 
-print (General_Table)
-print("*******************************")
-print (len(General_Table))
+# creater_new_animals (10, 60)
 
-for index in range (len(General_Table)):
-    print (index)
-    if General_Table[index][5] > 0:
-        print ("ok")
-        General_Table.pop (index)
-        print ("new long", len(General_Table))
-        print ("new index", index)
 
+# print (General_Table)
+# print("*******************************")
+# print (len(General_Table))
+# print("*******************************")
+
+# # for index  in range (len(General_Table)):
+# #             if General_Table [index][5] > 0:
+# #                 General_Table.pop(index)
+# #             index = index - 1
+
+
+# # тут обратная идея - нужно ставить не условие, по которому удаляется, а условие, по которому остаётся
+# Servis = list()
+# for index  in range (len(General_Table)):
+#     if General_Table [index][5] < 75:
+#         Servis.append(General_Table [index])
+# General_Table = Servis
+# Servis = []
+# print (Servis)
+
+# print (General_Table)
+# print ("***************")
+# print (len(General_Table))            
+            
+
+# for index in range (len(General_Table)):
+#     print ("index", index)
+#     # print("animal to the index", General_Table[index])
+#     if len(General_Table) <= index:
+#         break
+#     if General_Table[index][5] > 0:
+#         print ("ok")
+#         General_Table.pop (index)
+#         index = index - 1
+#     print ("new long", len(General_Table))
+#     print ("new index", index)
+#     print("animal to new index", General_Table[index])
+#     print("*******************************")
 
 
 # for index in range (len(General_Table)):
